ca71e4a2e80c12e5dd22503b9258fe12198f1c16
tahsin soufizade <soufilearn@gmail.com>
2024-12-25 16:18:42 +0330

    centeralize homeList title

--- components/homes/HomesList.jsx
+++ components/homes/HomesList.jsx
@@ -4,7 +4,9 @@ import Home from "./Home";
 function HomesList() {
   return (
     <div className="mx-10 mt-10">
-      <h2 className="mb-5 text-white">Homes For Sale</h2>
+      <h2 className="mb-5 text-white text-center md:text-left">
+        Homes For Sale
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {data.homes.map((home) => (
           <Home key={home.id} {...home} />
