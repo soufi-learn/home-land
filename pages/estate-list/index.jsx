e6603ea214711ef26f0f5f62993514c4715870ce
tahsin soufizade <soufilearn@gmail.com>
2025-01-02 15:09:51 +0330

    add sort input in homes page

--- pages/estate-list/index.jsx
+++ pages/estate-list/index.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 function EstateList() {
   const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
   const [homes, setHomes] = useState(data.homes);
 
   useEffect(() => {
@@ -13,24 +14,36 @@ function EstateList() {
     setHomes(newHomes);
   }, [search]);
 
-  const changeSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
   return (
     <div className="mx-10 mt-10">
-      <div className="flex justify-between ">
+      <div className="flex justify-between items-center mb-4">
         <h2 className="mb-5 text-white text-center md:text-left">
           Homes For Sale
         </h2>
-        <input
-          type="text"
-          placeholder="search"
-          className="px-2 py-1 rounded-md w-80 mb-4 outline-none shadow-md"
-          value={search}
-          onChange={changeSearch}
-        />
+        {/* Filter methods */}
+        <div className="flex items-center gap-4 flex-row-reverse">
+          <input
+            type="text"
+            placeholder="search"
+            className="px-2 py-[7px] rounded-md w-80 outline-none shadow-md"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+
+          <select
+            defaultValue={sort}
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 outline-none"
+          >
+            <option value="-1" selected>
+              sort homes
+            </option>
+            <option value="price">By Price</option>
+            <option value="rom-count">By room count</option>
+            <option value="meterage">By Meterage</option>
+          </select>
+        </div>
       </div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {homes.map((home) => (
           <Estate key={home.id} {...home} />
